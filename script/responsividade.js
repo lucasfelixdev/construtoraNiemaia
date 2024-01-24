bded4306c415bdc320263b70c63b6e0b8cd74cb6
Lukas Félix <lucas2009jesus@gmail.com>
2024-01-24 09:52:51 -0300

    ADD - Cache

--- script/responsividade.js
+++ script/responsividade.js
@@ -2,6 +2,10 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 15,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
--- script/script.js
+++ script/script.js
@@ -1,4 +1,37 @@
 
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/script.js', 
+  '/responsividade.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch(error => {
+      console.error('Erro ao registrar o Service Worker:', error);
+    });
+}
+
 
 
 window.revelar = ScrollReveal({
@@ -157,14 +190,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Calcula a porcentagem de rolagem
     var scrollPercentage = (scrollTop / (bodyHeight - windowHeight)) * 100;
+
     // Ajusta a opacidade com base na porcentagem de rolagem
-    var opacity = Math.min(1, scrollPercentage / 40); // Máximo de 1 de opacidade após 50% de rolagem
+    var opacity = Math.min(1, scrollPercentage / 6); // Máximo de 1 de opacidade após 50% de rolagem
     navbar.style.backgroundColor = `rgba(245, 245, 245, ${opacity})`;
 
    // ALTERA COR DOS LINKS DE ACORDO COM A ROLAGEM
 
 
-    var linkColor = `rgba(0, 0, 0, ${Math.min(1, scrollPercentage / 40)})`;
+    var linkColor = `rgba(0, 0, 0, ${Math.min(1, scrollPercentage / 5)})`;
     navlink.forEach(function (link) {
       link.style.color = linkColor;
     });
@@ -176,7 +210,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
     
     // Reverte a cor do texto para branco se a porcentagem de rolagem for menor que 50%
-    if (scrollPercentage <= 25) {
+    if (scrollPercentage <= 5) {
 
       burguernav.forEach(function (link) {
         link.style.backgroundColor = "#fff";
@@ -195,50 +229,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-tailwind.config = {
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: {"50":"#fff7ed","100":"#ffedd5","200":"#fed7aa","300":"#fdba74","400":"#fb923c","500":"#f97316","600":"#ea580c","700":"#c2410c","800":"#9a3412","900":"#7c2d12","950":"#431407"}
-      }
-    },
-    fontFamily: {
-      'body': [
-    'Montserrat Alternates', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ],
-      'sans': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ]
-    }
-  }
-}
+// tailwind.config = {
+//   darkMode: 'class',
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: {"50":"#fff7ed","100":"#ffedd5","200":"#fed7aa","300":"#fdba74","400":"#fb923c","500":"#f97316","600":"#ea580c","700":"#c2410c","800":"#9a3412","900":"#7c2d12","950":"#431407"}
+//       }
+//     },
+//     fontFamily: {
+//       'body': [
+//     'Montserrat Alternates', 
+//     'ui-sans-serif', 
+//     'system-ui', 
+//     '-apple-system', 
+//     'system-ui', 
+//     'Segoe UI', 
+//     'Roboto', 
+//     'Helvetica Neue', 
+//     'Arial', 
+//     'Noto Sans', 
+//     'sans-serif', 
+//     'Apple Color Emoji', 
+//     'Segoe UI Emoji', 
+//     'Segoe UI Symbol', 
+//     'Noto Color Emoji'
+//   ],
+//       'sans': [
+//     'Inter', 
+//     'ui-sans-serif', 
+//     'system-ui', 
+//     '-apple-system', 
+//     'system-ui', 
+//     'Segoe UI', 
+//     'Roboto', 
+//     'Helvetica Neue', 
+//     'Arial', 
+//     'Noto Sans', 
+//     'sans-serif', 
+//     'Apple Color Emoji', 
+//     'Segoe UI Emoji', 
+//     'Segoe UI Symbol', 
+//     'Noto Color Emoji'
+//   ]
+//     }
+//   }
+// }
 
